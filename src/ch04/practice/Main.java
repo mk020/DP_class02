879c31b4f48ed967a4ba8f779c171565278bc8d0
mk020 <mk02081@naver.com>
2023-03-28 11:47:08 +0900

    0328 수업내용 04 factory method

---
+++ src/ch04/practice/Main.java
@@ -0,0 +1,28 @@
+package ch04.practice;
+
+import ch04.practice.framework.Factory;
+import ch04.practice.framework.Product;
+import ch04.practice.idcard.IDCard;
+import ch04.practice.idcard.IDCardFactory;
+
+public class Main {
+  public static void main(String[] args) {
+    //1. 신분증 생산 (직접)
+    Product id01 = new IDCard("ymk");
+    id01.use();
+
+    //Product id = new IDCard(); 오류발생
+
+    //2. 신분증 생산 (공장을 통해서)
+    // Product id02 = 공장.create()
+    Factory f = new IDCardFactory();
+    Product id02 = f.create("ymk");
+    id02.use();
+
+
+
+
+
+
+  }
+}
---
+++ src/ch04/practice/framework/Product.java
@@ -0,0 +1,7 @@
+package ch04.practice.framework;
+
+public abstract class Product {
+    // 컴파일 오류
+    // public abstract Product(String name);
+    public abstract void use();
+}

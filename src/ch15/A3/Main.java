eec714ab7f85708cc1c05543669188439e196e2e
mk020 <mk02081@naver.com>
2023-05-30 10:43:56 +0900

    15 Facade 수업내용 0530

--- src/ch15/A3/Main.java
+++ src/ch15/A3/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         String title = "Welcome!";
         String message = "Hello, world!";
+        //큰따옴표 3개 -> text block 텍스트 블럭 , %s로 바뀌는 부분 설정.
         String html = """
         <!DOCTYPE html>
         <html>
@@ -14,7 +15,7 @@ public class Main {
                 <h1 style="text-align: center">%s</h1>
             </body>
         </html>
-        """.formatted(title, message);
+        """.formatted(title, message); // formatted ->  인자 값 하나씩 채워줌 
         System.out.print(html);
     }
 }
